5b64008c865e74f79bc4edb778dcc5037c0b616a
surajrjagtap <158743073+surajrjagtap@users.noreply.github.com>
2024-11-19 17:34:02 +0530

    DOM intro

---
+++ 01_dom.js
@@ -0,0 +1,19 @@
+//DOM
+// Document Object Model
+// overview
+    // DOM is a programming interface for HTML and XML documents. It represents the structure of a document as
+    // a tree-like data structure. This allows you to select and manipulate elements in the document using a
+    // variety of methods.
+// how to use
+//deep study
+
+
+// browser can add document object in window{} object
+
+console.log(window);
+// console.log(window.document);
+// console.dir(window.document);
+// console.dir(document);
+ console.log(document);
+
+    
