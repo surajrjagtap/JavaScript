f6d6afcb52718ff87882a071a3df6520fef0cebb
surajrjagtap <158743073+surajrjagtap@users.noreply.github.com>
2024-11-21 16:34:46 +0530

    get Element By Id

---
+++ 02_getElemetById.js
@@ -0,0 +1,6 @@
+// accessing any element like header ,div , h2
+// console.log( document.getElementById("main-heading"));
+
+// console.log(typeof document.getElementById("main-heading")); // Object
+const mainHeading = document.getElementById("main-heading")
+console.log( mainHeading);
