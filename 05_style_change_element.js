a166d0af0ef01ef54e4fbba6a5a0accfafaa5c6d
surajrjagtap <158743073+surajrjagtap@users.noreply.github.com>
2024-11-22 22:51:23 +0530

    style change element

---
+++ 05_style_change_element.js
@@ -0,0 +1,9 @@
+// change the style of elements
+
+const mainHeading  = document.querySelector("div.headline h2");
+// console.log(mainHeading.style); // set syle to the selected element
+mainHeading.style.color = "red"; // change the color of the text to red
+mainHeading.style.backgroundColor = "blue";
+mainHeading.style.border = "5px solid green";
+
+
